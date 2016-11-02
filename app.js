815ec891ecb4f9b93a22b459d2bfd3c70d3e3424
Axel <lezinaxel@gmail.com>
2016-11-02 09:28:46 +0100

    Ajout de l'affichage des erreurs

--- app.js
+++ app.js
@@ -12,25 +12,32 @@
 
 		},
 		clickValider : function(){
-		var userDay = $('#day').val();
-		var userMonth = $('#month').val();
-		var userYear = $('#year').val();
-		var userDate = moment({year : userYear , month : userMonth , day : userDay});
-		app.result = (moment(userDate).format('dddd'));
-		$('#showDay').html(app.result);
-		
-		
-		
+			var userDay = $('#day').val();
+			var userMonth = $('#month').val();
+			var userYear = $('#year').val();
+			var userDate = moment({year : userYear , month : userMonth , day : userDay});
+			app.result = (moment(userDate).format('dddd'));
+			$('#showDay').append(app.result);
+			if(userDay < 1 || userDay > 31){
+				$('#error').append('Le jour doit être compris entre 1 et 31');
+				$('#day').css('background-color', '#ff7473');
+			};
+			if (userYear < 0){
+				$('#error').append('L\'année doit être supérieure à 0');
+			}
+
+
+
 		},
 		printResult : function(){
-			
-			 
+
+
 		},
 	}
 
 	app.init();
 })();
-		
+
 
 
 
