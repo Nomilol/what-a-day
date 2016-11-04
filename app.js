9a886d99eb64f4d861161e6647bb415977c33468
Axel <lezinaxel@gmail.com>
2016-11-04 16:04:48 +0100

    Erreur ( avec l'overlay qui s'affiche toujours )

--- app.js
+++ app.js
@@ -19,31 +19,28 @@
 			var userYear = $('#year').val();
 			var userDate = moment({year : userYear , month : userMonth , day : userDay});
 			app.result = (moment(userDate).format('dddd'));
-			$('#overlay').show().html(app.result + "<button id='restart'> Recommencer </button>");
+			$('#overlay').show();
+			$('#overlay').html(app.result + "<button id='restart'> Recommencer </button>");
 			$('#hide').hide();
 			if(userDay < 1 || userDay > 31){
-				$('#error').show().append('Le jour doit être compris entre 1 et 31');
-				
+				$('#error').show();
+				$('#error').html('Le jour doit être compris entre 1 et 31');
 			};
 			if (userYear < 0){
-				$('#error').show().append('L\'année doit être supérieure à 0');
-			}
+				$('#error').show();
+				$('#error').html('L\'année doit être supérieure à 0');
+			};
+			//debugger
 		},
 		print : function(){
 			$('#error').hide();
 			$('#overlay').hide();
-
-
 		},
+
 	}
 
 	app.init();
 })();
-
-
-
-
-
 
 
 
